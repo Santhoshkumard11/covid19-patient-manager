e3065d4707d51c663c171be60ac97f3375653982
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2020-06-01 17:02:33 +0530

    Initial commit

--- app/src/main/java/com/example/covid19_patient_manager/Controller/DashboardActivity.kt
+++ app/src/main/java/com/example/covid19_patient_manager/Controller/DashboardActivity.kt
@@ -2,7 +2,12 @@ package com.example.covid19_patient_manager.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.covid19_patient_manager.R
+import kotlinx.android.synthetic.main.dashboard_layout.*
 
 class DashboardActivity : AppCompatActivity() {
 

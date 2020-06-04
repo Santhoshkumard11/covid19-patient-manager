6ecd7d06acf7674e37c5e294d310e0f7b2bfbcb2
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2020-06-05 01:40:14 +0530

    added the database activity

---
+++ app/src/main/java/com/example/covid19_patient_manager/Controller/DatabaseActivity.kt
@@ -0,0 +1,10 @@
+package com.example.covid19_patient_manager.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatabaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
